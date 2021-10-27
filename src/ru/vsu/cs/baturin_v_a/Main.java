e972c5569a6fe509d898c23508be291990ced432
CooIDragon <90614965+CooIDragon@users.noreply.github.com>
2021-10-27 15:01:36 +0300

    Update Main.java

--- src/ru/vsu/cs/baturin_v_a/Main.java
+++ src/ru/vsu/cs/baturin_v_a/Main.java
@@ -1,7 +1,10 @@
 package ru.vsu.cs.baturin_v_a;
+
 import java.util.Locale;
 import java.util.Scanner;
+
 public class Main {
+    
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
 
